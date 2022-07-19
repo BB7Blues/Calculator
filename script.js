6af8e875e83d2451b3d6a3ec1f096baf6d7c2e5e
BBBlues <t.fahrlaender@outlook.de>
2022-07-19 05:15:45 +0200

    Added Clear button funcionalty and a lot of stuff that i don't remember :(

--- script.js
+++ script.js
@@ -26,6 +26,34 @@ function operate(a,b,c){
 
 const numberPad = document.querySelector('.numbers');
 const operatorPad = document.querySelector('.operators');
+const specialPad = document.querySelector('.specialContain');
+const specialPad2 = document.querySelector('.specialContain2');
+
+for (let i = 0 ; i < 2;i++){
+    let knopf = document.createElement('div');
+    let num   = document.createElement('div');
+    knopf.classList.add('sButton');
+    num.classList.add('special');
+    knopf.setAttribute('id',"sBo" + i);
+    num.setAttribute('id', "sBottom" +i);
+    num.textContent = i;
+    specialPad2.appendChild(knopf);
+    knopf.appendChild(num);
+
+}
+
+for (let i = 0 ; i < 2;i++){
+    let knopf = document.createElement('div');
+    let num   = document.createElement('div');
+    knopf.classList.add('sButton');
+    num.classList.add('special');
+    knopf.setAttribute('id',"sB" + i);
+    num.setAttribute('id', "s" +i);
+    num.textContent = i;
+    specialPad.appendChild(knopf);
+    knopf.appendChild(num);
+
+}
 
 for (let i = 1 ; i < 10;i++){
     let knopf = document.createElement('div');
@@ -49,38 +77,139 @@ for (let i = 1 ; i < 5;i++){
     operatorPad.appendChild(knopf);
     knopf.appendChild(num);
 }
-
+let numberArrayNew = [];
+let numberArrayOld = [];
+let operatorArray = [];
+let fullNumbernew = 0;
+let fullNumberold = 0;
 const plus = document.getElementById('op1');
 const minus = document.getElementById('op2');
 const teilen = document.getElementById('op3');
 const mal = document.getElementById('op4');
+const clearBtn = document.getElementById('s0');
+const delBtn = document.getElementById('s1');
+const zero = document.getElementById('sBottom0');
+const equal = document.getElementById('sBottom1');
+zero.textContent = "0";
+equal.textContent   = "=";
+clearBtn.textContent = "AC";
+delBtn.textContent   = "DEL";
 plus.textContent = "+";
 minus.textContent = "-";
 teilen.textContent = "/";
-mal.textContent = "x";
+mal.textContent = "*";
 
+function clearDisplay(){
 
+    let everything = values.querySelectorAll('.displayValue');
+    everything.forEach(a =>{
+        values.removeChild(a);
+    });
+    numberArrayNew = [];
+    numberArrayOld = [];
+    operatorArray = [];
+    fullNumbernew = 0;
+    fullNumberold = 0;
+}
 
 
 const values = document.querySelector('.valueContainer');
-
+const operateButtons = document.querySelectorAll('.oButton');
+const acBtn = document.getElementById('s0');
+const zeroBtn = document.getElementById('sBo0');
+const equalBtn = document.getElementById('sBo1');
 const buttons = document.querySelectorAll('.nButton');
 
-buttons.forEach(btn => {
+
+acBtn.addEventListener('click', clearDisplay);
+zeroBtn.addEventListener('click', ()=> {
+
+    let text = document.getElementById('sBottom0');
+    let value = document.createElement('div');
+    value.classList.add('displayValue');
+    value.textContent = text.textContent;
+    numberArrayNew.push(value.textContent);
+    fullNumbernew = parseInt(numberArrayNew.reduce((total,num) => {
+      
+        return total  = total + num;
+    },0))
+
+    values.appendChild(value);
+
+
+});
+equalBtn.addEventListener('click', ()=> {
+   let value = document.createElement('div');
+    
+   
+   let everything = values.querySelectorAll('.displayValue');
+    everything.forEach(a =>{
+        values.removeChild(a);
+    });
+
+    value.classList.add('displayValue');
+    value.textContent = operate(fullNumberold,operatorArray,fullNumbernew);
+    fullNumbernew = operate(fullNumberold,operatorArray,fullNumbernew);
+    numberArrayNew = [];
+    numberArrayOld = [];
+    operatorArray = [];
+    numberArrayNew.push(fullNumbernew);
+    fullNumberold = 0;
+    values.appendChild(value);
+
+});
+
+operateButtons.forEach(btn => {
     
     btn.addEventListener('click', () =>{
-        let text = btn.querySelector('.nummer');
+        
         let value = document.createElement('div');
         value.classList.add('displayValue');
         value.textContent = btn.textContent;
         console.log(btn.textContent);
+        fullNumberold = fullNumbernew;
+        fullNumbernew = 0;
+        numberArrayOld = numberArrayNew;
+        numberArrayNew = [];
+        operatorArray.push(btn.textContent);
         values.appendChild(value);
     });
     
     
 });
 
+buttons.forEach(btn => {
     
+    btn.addEventListener('click', () =>{
+        
+        let value = document.createElement('div');
+        value.classList.add('displayValue');
+        value.textContent = btn.textContent;
+        numberArrayNew.push(value.textContent);
+        console.log(numberArrayNew);
+        console.log(numberArrayOld);
+        fullNumbernew = parseInt(numberArrayNew.reduce((total,num) => {
+         
+            return total  = total + num;
+        },0))
+        console.log(fullNumbernew);
+        console.log(fullNumberold);
+        values.appendChild(value);
+    });
+    
+    
+});
 
+/*function makeNumber(btn){
 
+    let value = document.createElement('div');
+    value.classList.add('displayValue');
+    value.textContent = btn.textContent;
+    //console.log(btn.srcElemetn);
+    values.appendChild(value);
 
+}
+    
+buttons.forEach(btn => btn.addEventListener('click', makeNumber(btn)));
+
+*/
